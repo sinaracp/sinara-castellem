73762de3ea1610a2554fa17ebd6f8708906dad7d
carlos Fernandes <carlosrf@gmail.com>
2017-01-17 00:47:11 +0100

    Adds e-mail feature and decrease header image size

--- mailer.php
+++ mailer.php
@@ -5,15 +5,15 @@
     $email_field = $_POST['email'];
     $phone_field = $_POST['phone'];
     $message = $_POST['message'];
-    $headers[] = "Content-Type: text/plain; charset=UTF-8"
-    $headers[] = "From: Contato do Site <contato@sinaracastellem.com>";
+    $headers = "Content-Type: text/plain; charset=UTF-8";
 
-    $subject = "Mensagem de $name_field/$email_field";
+    $subject = "Mensagem de $name_field ( $email_field )";
     $body = "From: $name_field\nE-Mail: $email_field\n\n Mensagem:\n $message";
 
     echo "Email foi enviado para $to!";
     mail($to, $subject, $body, $headers);
   } else {
-    echo "blarg!";
+    echo "Problema ao enviar E-mail para $to!";
   }
+  header("Location: http://sinaracastellem.com");
 ?>
